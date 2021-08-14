7f012a2287f5d88fd2d71aa715b34b6088dceee1
Arthur <arthrp@users.noreply.github.com>
2021-08-14 21:00:04 +0300

    Adding tests and renaming test

--- src/test.rs
+++ src/test.rs
@@ -1,10 +1,31 @@
 use super::*;
 
 #[test]
-fn parses_basic_cases(){
+fn parses_basic_strings() {
     let (_, c) = rgb_color("rgb(100,200,255)").unwrap();
 
     assert_eq!(c.red, 100);
     assert_eq!(c.blue, 255);
     assert_eq!(c.green, 200);
+
+    let (_, c1) = rgb_color("rgb(0,0,200)").unwrap();
+
+    assert_eq!(c1.red, 0);
+    assert_eq!(c1.blue, 200);
+    assert_eq!(c1.green, 0);
+}
+
+#[test]
+fn parses_strings_with_whitespaces() {
+    let (_, c) = rgb_color("rgb(100, 200, 255)").unwrap();
+
+    assert_eq!(c.red, 100);
+    assert_eq!(c.blue, 255);
+    assert_eq!(c.green, 200);
+
+    let (_, c1) = rgb_color("rgb(0,    0,   200)").unwrap();
+
+    assert_eq!(c1.red, 0);
+    assert_eq!(c1.blue, 200);
+    assert_eq!(c1.green, 0);
 }
